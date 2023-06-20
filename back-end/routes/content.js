f5faad8451afff7f668c28470f6a1d6f88eaa130
Lee <jihyeule@amazon.com>
2023-06-20 14:40:31 -0400

    installed express, multer, jwt and now able to listen on port 3000

--- back-end/routes/content.js
+++ back-end/routes/content.js
@@ -5,10 +5,5 @@ const multer = require("../middleware/multer-config");
 
 const contentCtrl = require("../controllers/content");
 
-router.post("/", auth, multer, contentCtrl.createContent); //create content
-router.get("/", auth, contentCtrl.getAllContent); //show all content
-router.put("/:id", auth, multer, contentCtrl.modifyContent); //edit text or reload multimedia
-router.delete("/:id", auth, contentCtrl.deleteContent); //delete text or multimedia
-
 
 module.exports = router;

cdf1981b954240bbbdbd51455bade4f42bc54f0a
Lee <jihyeule@amazon.com>
2023-06-25 23:14:58 -0400

    front-end for Single Page app is ready

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hi</h1>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
